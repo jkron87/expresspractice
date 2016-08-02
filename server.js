a1e7191c6c8a931047e0b1a5d1c32fa602a64262
Jennifer Kron <jenniferkron@gmail.com>
2016-08-02 14:51:11 -0400

    modified porter

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/', function (request, response) {
 
 //then start Server
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
